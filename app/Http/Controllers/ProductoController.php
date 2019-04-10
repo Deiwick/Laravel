95cf4939a7a3f9ce379ef3acbcbca6fba377a6aa
David Cáceres <dcacerbe@outlook.com>
2019-04-10 17:14:23 -0400

    Mostrar Listado de productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Producto;
 
 class ProductoController extends Controller
 {
@@ -10,6 +11,7 @@ class ProductoController extends Controller
 
     public function index()
     {
-        return view('productos.index');
+        $productos = Producto::orderBy('id','ASC')->paginate();
+        return view('productos.index',compact('productos'));
     }
 }
--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -2,7 +2,37 @@
 
 @section('content')
     <div class="col-sm-8">
+        <h2> 
+            Listado de productos:
+        </h2>
+        <table class="table table-hover table-striped">
+            <thead>
+                <tr>
+                    <td width="20px">ID</td>
+                    <td>Nombre</td>
+                    <td>Descripcion</td>
+                    <td>Stock</td>
+                    <td colspan="3">&nbsp;</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($productos as $producto)
+                    <tr>
+                        <td>{{ $producto->id }} </td>
+                        <td>{{ $producto->nombre }} </td>
+                        <td>{{ $producto->descripcion }} </td>
+                        <td>{{ $producto->stock }} </td>
+                        <td> <a>Ver</a> </td>
+                        <td> <a>Modificar</a> </td>
+                        <td> <a>Borrar</a> </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        {!! $productos->render() !!}
     </div>
+
     <div class="col-sm-4">
         chao cabros suscribanse
     </div>
