827263131efac36b1c63ade830d1e4b79e15b436
David Cáceres <dcacerbe@outlook.com>
2019-04-10 16:07:29 -0400

    Creacion de la migracion, modelo y seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,3 +24,13 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+
+$factory->define(App\Producto::class, function (Faker $faker) {
+    return [
+        'nombre' => $faker->sentence(2),
+        'descripcion' => $faker->sentence(5),
+        'stock' =>  $faker->randomNumber(),
+
+    ];
+});
+
